b3fb48418de340a92417296fef41f9b6e37a573b
gilleswillemot <gilles.willemot@gmail.com>
2018-05-08 13:40:27 +0200

    user module interceptor change

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -11,7 +11,7 @@ import { LogoutComponent } from './logout/logout.component';
 import { UserInformationComponent } from './user-information/user-information.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 //import { UserDataService } from '../services/user-data.service';
-import { basehttpInterceptorProviders } from '../http-interceptors';
+// import { basehttpInterceptorProviders } from '../http-interceptors';
 
 const routes = [
   { path: 'login', component: LoginComponent },
@@ -35,7 +35,7 @@ const routes = [
     UserInformationComponent
   ],
   providers: [
-    AuthenticationService,basehttpInterceptorProviders,
+    AuthenticationService,/*,basehttpInterceptorProviders,*/
     AuthGuardService
   ],
   exports: [

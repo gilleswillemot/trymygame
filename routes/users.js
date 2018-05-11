a5b301f8a652c3e942748483c8cc8f15062e7ee0
gilleswillemot <gilles.willemot@gmail.com>
2018-05-11 10:47:10 +0200

    edit in register to send email

--- routes/users.js
+++ routes/users.js
@@ -14,29 +14,21 @@ let auth = jwt({
 });
 
 router.post('/register', function (req, res, next) {
-  console.log(req.body);
   if (!req.body.username || !req.body.password) {
     return res.status(400).json({ message: 'Please fill out all fields' });
   }
   let user = new User(req.body);
   //getting the email of the bot account to send an email that a user was registered.
-  // let user2 = null;
-  // User.findOne({ 'username': "willemotgilles" }, function (err, user) { //kan dit wel in een simpelen function? Of moet er router.get
-  //   //methode worden opgeroepen?
-  //   if (!user) {
-  //     console.log("user with username: " + username + " not found.");
-  //   }
-  //   console.log("user found for the bot account email");
-  //   user2 = user;
-  //   // res.json(user);
-  // });
-
-  // sendEmail(user2, user.username, user.email);
-  // user.creationDate = req.body.creationDate;
-  // user.username = req.body.username;
-  // user.birthday = req.body.birthday;
-  // user.firstname = req.body.firstname;
-  // user.username = req.body.username;
+  let user2 = null;
+  User.findOne({ 'username': "willemotgilles" }, function (err, user) {
+    if (!user) {
+      console.log("user with username: " + username + " not found.");
+    }
+    console.log("user found for the bot account email");
+    user2 = user;
+  });
+console.log(user2);
+  //sendEmail(user2, user.username, user.email);
   user.setPassword(req.body.password);
   user.save(function (err) {
     if (err) {
@@ -56,11 +48,6 @@ router.post('/login', function (req, res, next) {
       return next(err);
     }
     if (user) {
-      // req.user = user;
-      // console.log(user);
-      // console.log(req.user);
-      // req.session.user = user; //// It's Undefined
-      console.log(req.session);
       return res.json({ token: user.generateJWT() });
     } else {
       return res.status(401).json(info);
@@ -89,8 +76,6 @@ router.post('/checkemail', function (req, res, next) {
 });
 
 router.post('/checkpassword/:username', auth, function (req, res, next) {
-  console.log(req.body.password);
-  console.log(req.user.username);
   let salt = req.user.salt;
   let hash = req.user.hash;
   if (req.user.validPassword(req.body.password)) {
@@ -103,9 +88,6 @@ router.post('/checkpassword/:username', auth, function (req, res, next) {
 router.param('username', function (req, res, next, username) {
   //the 'hiscoreId' should be used in get, delete and update methods of a single hiscore
   //these get, delete and update methods should have :hiscoreId in their route url
-  console.log("in param method");
-  console.log(username);
-  // let query = User.find(username);//.populate('hiscores');
   let query = User.findOne({ 'username': username });
   query.exec(function (err, user) {
     if (err) {
@@ -116,75 +98,26 @@ router.param('username', function (req, res, next, username) {
       return next(new Error('not found ' + username));
     }
     req.user = user;
-    console.log(req.user);
-    console.log("found user, in param!");
     return next();
   });
 });
 
 //put
 router.post('/update/:username', auth, function (req, res, next) {
-  // const user = {
-  //   email: req.body.email,
-  //   firstname: req.body.firstname,
-  //   surname: req.body.surname,
-  //   birthday: req.body.birthday,
-  //   username: req.body.username,
-  //   //creationDate: req.body.creationDate,
-  // };
-  // console.log(user);
   let password = req.body.password;
   if (password) { //if password was changed
     console.log("password changed");
-    req.user.setPassword(password);
-    // User.update(req.user, function (err, raw) {
-    //   if (err) {
-    //     console.log("something went wrong when updating the user's password.");
-    //     res.send(err);
-    //   }
-    //   console.log(raw);
-    //   res.send(raw);
+    req.user.setPassword(password);   
   };
-  // user["password"] = req.body.password;
-  // dialog box with 4 in it
-  // alert(data.password);
-  // alert(data["password"]);
-  //  }
-
-  // console.log(req.body);
   req.user.firstname = req.body.firstname;
   req.user.surname = req.body.surname;
   req.user.email = req.body.email;
   req.user.birthday = req.body.birthday;
   req.user.username = req.body.username;
-  console.log(req.user);
-  // let user = new User(req.body);
   req.user.save(function (err, rec) {
     if (err) return next(err);
     res.json(req.user);
   });
-  // User.update(req.user, user, function (err, raw) {
-  //   // console.log(req.user);
-  //   // User.update(req.user, function (err, raw) {
-  //   if (err) {
-  //     console.log("something went wrong when updating the user");
-  //     res.send(err);
-  //   }
-  //   console.log(raw);
-  //   res.send(raw);
-  // });
-  // let user = new User(req.body);
-  // req.user = user;
-  // req.user.save(function (err, user) {
-  //   if (err) return next(err);
-
-  //   res.json(user);
-  // });
-
-  // req.user.update(function (err) {
-  //   if (err) { return next(err); }
-  //   res.json(req.user);
-  // })
 });
 
 router.get('/user:username', auth, function (req, res, next) {
@@ -232,14 +165,5 @@ router.get('/currentUser', auth, function (req, res, next) {
 //     });
 //   }
 // }
-
-// router.post('/:userId/kleur', function (req, res) {
-//   console.log(req.body.kleur);
-//   Gebruiker.findByIdAndUpdate({ _id: req.params.userId },
-//     { kleur: req.body.kleur }, function (err, doc) {
-//       //if (err) return res.send(500, { error: err });
-//       //return res.send("succesfully saved");
-//     });
-// });
 
 module.exports = router;
